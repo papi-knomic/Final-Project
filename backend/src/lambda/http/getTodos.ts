cc2e153f38c22bdfdaf470ffef343cf4e7627959
papi-knomic <knomic10@gmail.com>
2022-07-28 14:11:49 +0100

    first commit

---
+++ backend/src/lambda/http/getTodos.ts
@@ -0,0 +1,28 @@
+import 'source-map-support/register'
+//import * as AWS from 'aws-sdk'
+//import * as AWSXRay from 'aws-xray-sdk'
+import {APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler} from 'aws-lambda';
+import {getAllToDo} from "../../businessLogic/ToDo";
+
+
+//const XAWS = AWSXRay.captureAWS(AWS);
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    // TODO: Get all TODO items for a current user
+    console.log("Processing Event ", event);
+    const authorization = event.headers.Authorization;
+    const split = authorization.split(' ');
+    const jwtToken = split[1];
+
+    const toDos = await getAllToDo(jwtToken);
+
+    return {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+            "items": toDos,
+        }),
+    }
+};
---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'ohx6gi6g40';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'knomic.us.auth0.com',
+  clientId: 'kD2wJn0ckEgksKBce2NZDRKp7JiN5qxV',
+  callbackUrl: 'http://localhost:3000/callback'
+};
